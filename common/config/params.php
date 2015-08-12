1983314487c26acc819e7b41b826fd6bcea814fd
dingwen <dingwen@vip.qq.com>
2015-08-12 21:06:07 +0800

    modified:   common/config/params.php 	deleted:    frontend/runtime/cache/96/963454f612a8b5fb4a63ba1e97f028a1.bin 	deleted:    frontend/runtime/logs/app.log

--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,6 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+    'uploadPath' => '/home/wwwroot/cms/frontend/web/uploads/',
+    'uploadUrl' => 'http://www.qiz35.com/uploads/',
 ];
